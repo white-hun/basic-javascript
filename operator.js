1c3f3bed7260ed9a6b821d65ee255db26797c597
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-06 18:03:06 +0900

    Update Quiz

--- operator.js
+++ operator.js
@@ -172,3 +172,20 @@ for (let i = 0; i < 10; i++) {
     console.log(`i: ${i}, j: ${j}`);
   }
 }
+
+// break, continue
+// Q1. iterate from 0 to 10 and print only even numbers (use continue)
+for (let i = 0; i < 11; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+  console.log(`i: ${i}`);
+}
+
+// Q2. iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i < 11; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(`i: ${i}`);
+}
