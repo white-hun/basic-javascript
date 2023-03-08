279e8416021ec4a9d0a5ad181fbd2c23ce44cf07
white-hun <121800818+white-hun@users.noreply.github.com>
2023-03-08 12:06:42 +0900

    Update array-document.js

--- array-document.js
+++ array-document.js
@@ -1,7 +1,21 @@
 "use strict";
 
-const color = ["red", "orange"];
+const color = ["red", "orange", "yellow"];
 console.log(color);
 console.log(color.length);
 console.log(color.toString());
 console.log(color.toLocaleString());
+console.log(color.pop("red"));
+console.log(color.pop());
+console.log(color.push("orange", "yello", "green", "blue"));
+console.log(color);
+const color2 = ["pupple", "navy"];
+const newColor = color.concat(color2);
+console.log(newColor);
+console.log(color.join(" | "));
+console.log(color.reverse().join(", "));
+console.log(color.shift());
+console.log(color.reverse());
+console.log(color.push("blue"));
+console.log(color);
+console.log(color.slice(1, 3)); // index 1부터 3앞까지 출력
